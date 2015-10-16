312a9e1aac07c6f6636482790a25e6d4f1456edd
Johnny <s850042002@gmail.com>
2015-10-16 16:53:12 +0800

    1. update content 2. switch languages button 3. Minor changed of design

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var enJSON = require('../lang/en.json');
-var twJSON = require('../lang/tw.json');
+var enJSON = require('../lang/en-US.json');
+var twJSON = require('../lang/zh-TW.json');
 
-router.get('/lang', function(req, res){
-  if(req.query.lang === "en"){
-    res.send(JSON.stringify(enJSON));
+router.get('/switchlanguage', function(req, res){
+  if(req.query.lang === "zh-TW"){
+    res.send(JSON.stringify(twJSON));
   }
   else{
-    res.send(JSON.stringify(twJSON));
+    res.send(JSON.stringify(enJSON));
   }
 });
 
-router.get('/', function(req, res) {
-    console.log(req.user);
+router.get('/detectlanguage', function(req, res){
+  var lang = req.header("Accept-Language");
+  if(lang.indexOf("zh-TW") !== -1){
+    res.send(JSON.stringify(twJSON));
+  }
+  else{
+    res.send(JSON.stringify(enJSON));
+  }
 });
 
 module.exports = router;
